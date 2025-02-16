38c6fb17bc6bb2497cc81b7f2b82a848a37c33af
Vinicius Maia <viniciusmaia108@gmail.com>
2025-02-16 22:27:14 +0100

    Conclusion of chapter 1

--- temp.R
+++ temp.R
@@ -1,12 +1,20 @@
 library(devtools)
+
+# Setup
+create_package()
+use_git()
+use_mit_license()
+use_testthat()
+use_github()
+use_readme_rmd()
+
+# Add functions dependencies and tests
+use_r()
+use_test()
+use_package()
+
+# During development
+load_all()
 document()
 test()
 check()
-
-create_github_token()
-
-gitcreds::gitcreds_set()
-
-use_github()
-
-use_readme_rmd()
